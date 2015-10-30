5314a9f4235edeac4320ee7fe28a21aad7a8d583
Fedomn <shadowforfiend@gmail.com>
2015-10-30 18:01:07 +0800

    init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,92 @@
+class UsersController < ApplicationController
+
+  # GET users
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
+  end
+
+  # GET /users/show/1
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+
+
+  # GET /users/new
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User create success' }
+        format.json { render json: @user, status: create, location: @user }
+      else
+        format.html { render :action => 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  #PUT /users/1
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      # if @user.update_attributes(params[:user])
+      if @user.update_attributes(user_params)
+        format.html { redirect_to @user, notice: 'User update success' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  #DELETE /user/1
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
+
+
+
+
+
+
