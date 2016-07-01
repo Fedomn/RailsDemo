0a947d9e7dc6b07f62ed0df23a29669e18bb80a5
Fedomn <shadowforfiend@gmail.com>
2016-07-01 10:05:10 +0800

    update add authenticate in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate
+
+  def authenticate
+    if session[:username].blank?
+      if request.xhr?
+        render js: %(window.location.pathname="#{root_path}")
+      else
+        redirect_to(url_for(controller: 'welcome', action: 'login'), notice: t('general.not_authenticated'))
+      end
+    end
+  end
+
 end
